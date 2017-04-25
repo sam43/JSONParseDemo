4f085137025ca79870716d17b16569c3ebd95d51
Asif Ikbal <asif@Asif-7.local>
2017-04-25 19:44:15 +0600

    Initial Commit

---
+++ JsonDemo3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  JsonDemo3
+//
+//  Created by Asif Ikbal on 4/25/17.
+//  Copyright © 2017 Asif Ikbal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
